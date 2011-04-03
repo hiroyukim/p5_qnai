74a10a7dc7ea9818183367bae95add1d997531cd
山中裕之 <hiroyukimm@gmail.com>
2011-04-03 23:30:49 +0900

    初期化

---
+++ lib/Qnai.pm
@@ -0,0 +1,32 @@
+package Qnai;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Qnai -
+
+=head1 SYNOPSIS
+
+  use Qnai;
+
+=head1 DESCRIPTION
+
+Qnai is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
