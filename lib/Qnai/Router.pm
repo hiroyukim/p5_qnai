0683aa9cfa6604cc1eaff1abcceddf4be229c459
hiroyuki <hiroyuki@norn.niflheimr.org>
2011-05-05 15:01:14 +0900

    parse_pathを追加

--- lib/Qnai/Router.pm
+++ lib/Qnai/Router.pm
@@ -3,15 +3,26 @@ use strict;
 use warnings;
 use base qw/Class::Accessor::Fast/;
 use Router::Simple;
+use Params::Validate qw(:all);
+use Carp ();
 
-__PACKAGE__->mk_accessors(qw/rules router_simple/);
+__PACKAGE__->mk_accessors(qw/rules router_simple system_template/);
 
 sub new {
-    my ($class,$rules) = @_;
+    my $class = shift;
+    my ($template_path,$rules) = validate_pos(@_,
+        { type => SCALAR   },
+        { type => ARRAYREF },
+    );
+
+    Carp::Confess('please use Qnai::Router::Rule.') 
+        unless( @$rules == grep { $_->isa('Qnai::Router::Rule') } @$rules);
 
     my $self = bless {
-        rules         => $rules, 
-        router_simple => Router::Simple->new,
+        rules           => $rules, 
+        router_simple   => Router::Simple->new,
+        system_template => [],
+        template_path   => $template_path,
     }, $class;
 
     $self->_init();
@@ -23,12 +34,13 @@ sub _init {
     my $self = shift;
 
     for my $rule ( @{$self->rules} ) {
+
+        if( $rule->template ) {
+            push @{$self->{system_template}},$rule->template;
+        }
+
         $self->router_simple->connect(
-            $rule->{pattern} => {
-                pattern   => $rule->{pattern},
-                code      => $rule->{code},
-                opt       => $rule->{opt},
-            }
+            $rule->pattern => $rule->to_hash(), 
         );
     }
 }
@@ -36,14 +48,51 @@ sub _init {
 sub match {
     my $self = shift;
     my $env  = shift;
+
+    if( $self->is_directory_traversal($env) ) {
+        Qnai::Exception::DirectoryTraversal->throw();
+    }
+
     my $match_rule = $self->router_simple->match($env);
 
     if( $match_rule ) {
         return $match_rule;
     }
     else {
-        # default
+        if( $self->is_system_template($env) ) {
+            Qnai::Exception::SystemTemplate->throw();
+        }
+        else {
+            my ($dir,$file) = $self->parse_path($env);
+
+            my $template_path = join('/',$dir,$file);
+        } 
     }
+}
+
+sub is_directory_traversal {
+    my ($self,$path) = @_;    
+    ( $path =~ /\.{1,2}/g )
+}
+
+sub parse_path {
+    my ($self,$path) = @_;
+    my ( $dir,$file ) = $path =~ m{^/([a-z0-9_/]*)/([^/]*)$}i;
+    unless( $dir ) {
+        $dir = 'root'; 
+    }
+
+    unless( $file ) {
+        $file = 'index';
+    }
+
+    return ([split(/\//,$dir)],$file);
+}
+
+sub is_system_template {
+    my $self = shift;
+    my $template_path = shift;
+    ( grep { $template_path eq $_ } @{$self->system_template} ) ? 1 : 0;
 }
 
 1;
---
+++ t/Qnai-Router/base.t
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+use Test::More;
+use Qnai::Router;
+use Qnai::Router::Rule;
+
+my $rules = [
+    Qnai::Router::Rule->new({
+        pattern => '/',
+        code    => sub {
+            my $self = shift;
+
+            return 1;
+        },
+        template => '/root/index.html',
+    }),
+];
+
+my $template_path = './template/'; 
+
+my $router = Qnai::Router->new( $template_path => $rules );
+
+ok $router;
+
+subtest 'include system_template' => sub {
+    cmp_ok( $rules->[0], 'cmp', $router->system_template->[0] );
+    done_testing();
+};
+
+subtest 'match /' => sub {
+    ok $router->match({ PATH_INFO => '/' });
+    done_testing();
+};
+
+subtest 'unmatch /hoge/' => sub { 
+    ok( not $router->match({ PATH_INFO => '/hoge/' }) );
+    done_testing();
+};
+
+subtest 'parse_path /user/edit' => sub {
+    my ($dir,$file) = $router->parse_path('/user/edit');
+    cmp_ok( $dir->[0],  'eq', 'user' ); 
+    cmp_ok( $file, 'eq', 'edit'  ); 
+};
+
+subtest 'parse_path /hoge/hoge/hoge/hoge/' => sub {
+    my ($dir,$file) = $router->parse_path('/hoge/hoge/hoge/hoge/');
+    cmp_ok( join('/',@$dir), 'eq', 'hoge/hoge/hoge/hoge' ); 
+    cmp_ok( $file, 'eq', 'index'  ); 
+};
+
+subtest 'parse_path /' => sub {
+    my ($dir,$file) = $router->parse_path('/');
+    cmp_ok( $dir->[0],  'eq', 'root' ); 
+    cmp_ok( $file, 'eq', 'index'  ); 
+};
+
+done_testing();
